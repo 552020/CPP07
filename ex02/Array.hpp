f139262d830c79b4446b276a36dff063c4289288
552020 <stefanolombardo@posteo.de>
2024-03-18 22:28:39 +0100

    add tests for compex types

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,71 +1,41 @@
-#ifndef ARRAY_H
-#define ARRAY_H
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
 
+#include <exception>
 #include <stdexcept>
+#include <string>
 
 template <typename T>
 class Array
 {
   private:
-	T *elements;
+	T *_array;
 	unsigned int _size;
 
   public:
-	Array() : elements(NULL), _size(0)
-	{
-	}
+	Array();
+	// alternative: explicit Array(unsinged int)
+	Array(unsigned int n);
+	Array(const Array &other);
+	Array &operator=(const Array &other);
+	~Array();
+	// subscript operator
+	const T &operator[](unsigned int index) const;
+	T &operator[](unsigned int index);
 
-	explicit Array(unsigned int n) : _size(n)
-	{
-		// Here, new T[n]() dynamically allocates an array of n elements of type T, and importantly, it uses parentheses
-		// (()) to ensure that each element is default-initialized. For primitive types like int, float, etc., this
-		// means they are initialized to 0 or their equivalent zero-value. For class types, their default constructor is
-		// called.
-		// Alsternative:
-		// elements = new T[n];
-		elements = new T[n]();
-	}
-
-	Array(const Array &other) : _size(other._size), elements(new T[other._size])
-	{
-		for (unsigned int i = 0; i < _size; ++i)
-		{
-			elements[i] = other.elements[i]; // Direct copy of elements
-		}
-	}
-
-	~Array()
-	{
-		delete[] elements;
-	}
-
-	Array &operator=(const Array &other)
-	{
-		if (this != &other)
-		{
-			delete[] elements;
-			_size = other._size;
-			elements = new T[_size]();
-			for (unsigned int i = 0; i < _size; ++i)
-			{
-				elements[i] = other.elements[i];
-			}
-		}
-		return *this;
-	}
-	// subscript operator [] is overloaded to provide access to the elements of the array
-	T &operator[](unsigned int index)
-	{
-		if (index >= _size)
-			// throw std::out_of_range("Index out of bounds");
-			throw std::exception("Index out of bounds");
-		return elements[index];
-	}
-
-	unsigned int size() const
-	{
-		return _size;
-	}
+	unsigned int size() const;
 };
 
-#endif // ARRAY_H
+#include "Array.tpp"
+
+#endif
+
+// Note 1: Paremeterized constructor: elements = new T[n]();
+/*
+Here, new T[n]() dynamically allocates an array of n elements of type T, and importantly, it uses parentheses(()) to
+ensure that each element is default-initialized. For primitive types like int, float, etc., this means they are
+initialized to 0 or their equivalent zero-value. For class types, their default constructor is called.
+
+Alsternative:
+elements = new T[n];
+*/
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,80 +1,218 @@
 #include "Array.hpp"
 #include <iostream>
 
-int main()
+#define SIZE 10
+
+struct ComplexType
 {
+	std::string text;
+	int number;
+
+	ComplexType(std::string t = "42", int n = 42) : text(t), number(n)
+	{
+	}
+};
+
+void printComplexType(const ComplexType &ct)
+{
+	std::cout << "{" << ct.text << ", " << ct.number << "}";
+}
+
+// Output operator for ComplexType
+std::ostream &operator<<(std::ostream &os, const ComplexType &ct)
+{
+	return os << "{" << ct.text << ", " << ct.number << "}";
+}
+
+template <typename T>
+void printArray(const Array<T> &array)
+{
+	for (unsigned int i = 0; i < array.size(); ++i)
+	{
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+	if (array.size() == 0)
+	{
+		std::cout << "Empty array" << std::endl;
+	}
+}
+
+void printArray(const Array<ComplexType> &array)
+{
+	for (unsigned int i = 0; i < array.size(); ++i)
+	{
+		printComplexType(array[i]);
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	if (array.size() == 0)
+		std::cout << "Empty array" << std::endl;
+}
+
+void testComplexType()
+{
+
+	std::cout << "***Test ComplexType***\n" << std::endl;
 	// Test default constructor
-	Array<int> intArray;
-	std::cout << "Empty int array size: " << intArray.size() << std::endl;
-
-	// Test sized constructor and element access
-	Array<double> doubleArray(5);
-	std::cout << "Double array size: " << doubleArray.size() << std::endl;
-	for (unsigned int i = 0; i < doubleArray.size(); ++i)
-	{
-		std::cout << doubleArray[i] << " ";
-	}
-	std::cout << std::endl;
-
+	std::cout << "***Test default constructor***\n" << std::endl;
+	Array<ComplexType> complexArray;
+	std::cout << "complexArray size: " << complexArray.size() << std::endl;
+	std::cout << "complexArray elements: ";
+	printArray(complexArray);
+	// Test sized constructor
+	std::cout << "***Test sized constructor***\n" << std::endl;
+	Array<ComplexType> complexArraySized(SIZE);
+	std::cout << "complexArraySized size: " << complexArraySized.size() << std::endl;
+	std::cout << "complexArraySized elements: ";
+	printArray(complexArraySized);
 	// Test copy construction
-	Array<double> copiedArray = doubleArray;
-	// Modify the original array
-	for (unsigned int i = 0; i < doubleArray.size(); ++i)
+	Array<ComplexType> complexArrayCopy = complexArraySized;
+	std::cout << "complexArrayCopy just after copy construction: ";
+	printArray(complexArrayCopy);
+	for (unsigned int i = 0; i < complexArrayCopy.size(); ++i)
 	{
-		doubleArray[i] = i * 1.1;
+		std::string text = "new_" + std::to_string(i);
+		complexArrayCopy[i] = ComplexType(text, i);
 	}
+	std::cout << "complexArrayCopy after modification: ";
+	printArray(complexArrayCopy);
+	std::cout << "complexArraySized after modification of complexArrayCopy: ";
+	printArray(complexArraySized);
 
-	// Checking copied array remains unchanged
-	std::cout << "Copied array after modifying the original: ";
-	for (unsigned int i = 0; i < copiedArray.size(); ++i)
+	// Test assignment operator
+	std::cout << "***Test assignment operator***\n" << std::endl;
+	Array<ComplexType> anotherComplexArray(SIZE);
+	std::cout << "anotherComplexArray size: " << anotherComplexArray.size() << std::endl;
+	for (unsigned int i = 0; i < anotherComplexArray.size(); ++i)
 	{
-		std::cout << copiedArray[i] << " ";
+		std::string text = "another_" + std::to_string(i);
+		anotherComplexArray[i] = ComplexType(text, i);
 	}
-	std::cout << std::endl;
+	std::cout << "anotherComplexArray elements: ";
+	printArray(anotherComplexArray);
+	complexArrayCopy = anotherComplexArray;
+	std::cout << "complexArrayCopy after assignment: ";
+	printArray(complexArrayCopy);
+	for (unsigned int i = 0; i < complexArrayCopy.size(); ++i)
+	{
+		std::string text = "new_" + std::to_string(i);
+		complexArrayCopy[i] = ComplexType(text, i);
+	}
+	std::cout << "complexArrayCopy after modification: ";
+	printArray(complexArrayCopy);
+	std::cout << "anotherComplexArray after modification of complexArrayCopy: ";
+	printArray(anotherComplexArray);
 
 	// Test out of bounds access
+	std::cout << "***Test out of bounds access***\n" << std::endl;
 	try
 	{
-		std::cout << copiedArray[copiedArray.size()] << std::endl;
+		std::cout << complexArrayCopy[complexArrayCopy.size()] << std::endl;
 	}
 	catch (std::exception &e)
 	{
 		std::cout << "Caught exception: " << e.what() << std::endl;
 	}
-
-	Array<std::string> stringArray(3);
-	stringArray[0] = "Hello";
-	stringArray[1] = "World";
-	stringArray[2] = "!";
-	std::cout << "String array contents: ";
-	for (unsigned int i = 0; i < stringArray.size(); ++i)
+	catch (std::out_of_range &e)
 	{
-		std::cout << stringArray[i] << " ";
+		std::cout << "Caught out_of_range exception: " << e.what() << std::endl;
 	}
+}
+
+void testSimpleTypes()
+{
+	std::cout << "***Test simple types***\n" << std::endl;
+	// Test default constructor
+	std::cout << "***Test default constructor***\n" << std::endl;
+	Array<int> intArray;
+	std::cout << "intArray size: " << intArray.size() << std::endl;
+	std::cout << "intArray elements: ";
+	printArray(intArray);
 	std::cout << std::endl;
 
-	Array<int> copiedArray = intArray;
-	std::cout << "Copied array size: " << copiedArray.size() << std::endl;
-	for (unsigned int i = 0; i < copiedArray.size(); ++i)
-	{
-		std::cout << "copiedArray[" << i << "] = " << copiedArray[i] << std::endl;
-	}
+	// Test sized constructor
+	std::cout << "***Test sized constructor***\n" << std::endl;
+	Array<float> floatArray(SIZE);
+	std::cout << "floatArray size: " << floatArray.size() << std::endl;
+	std::cout << "floatArray elements: ";
+	printArray<float>(floatArray);
+	std::cout << std::endl;
 
-	// Modify original array to prove deep copy
-	intArray[0] = 42;
-	std::cout << "After modification, intArray[0] = " << intArray[0] << ", copiedArray[0] = " << copiedArray[0]
-			  << std::endl;
+	// Test copy construction
+	Array<float> floatArrayCopy = floatArray;
+	std::cout << "floatArrayCopy just after copy construction: ";
+	printArray<float>(floatArrayCopy);
+	for (unsigned int i = 0; i < floatArray.size(); ++i)
+		floatArray[i] = i * 1.1;
+	std::cout << "floatArray after modification: ";
+	printArray<float>(floatArray);
+	std::cout << "floatArrayCopy after modification of original: ";
+	printArray<float>(floatArrayCopy);
+	std::cout << std::endl;
 
-	// Test out-of-bounds access
+	// Test assignment operator
+	std::cout << "***Test assignment operator***\n" << std::endl;
+	Array<double> doubleArray(SIZE);
+	Array<double> anotherDoubleArray(SIZE);
+	std::cout << "doubleArray size: " << doubleArray.size() << std::endl;
+	for (unsigned int i = 0; i < doubleArray.size(); ++i)
+		doubleArray[i] = i * 1.5;
+	std::cout << "doubleArray elements: ";
+	printArray<double>(doubleArray);
+	anotherDoubleArray = doubleArray;
+	std::cout << "anotherDoubleArray after assignment: ";
+	printArray<double>(anotherDoubleArray);
+	for (unsigned int i = 0; i < anotherDoubleArray.size(); ++i)
+		anotherDoubleArray[i] = i * 2.5;
+	std::cout << "anotherDoubleArray after modification: ";
+	printArray<double>(anotherDoubleArray);
+	std::cout << "doubleArray after modification of anotherDoubleArray: ";
+	printArray<double>(doubleArray);
+
+	// Test out of bounds access
+	std::cout << "***Test out of bounds access***\n" << std::endl;
 	try
 	{
-		std::cout << "Attempting out-of-bounds access: ";
-		std::cout << intArray[intArray.size()] << std::endl;
+		std::cout << floatArrayCopy[floatArrayCopy.size()] << std::endl;
 	}
-	catch (const std::exception &e)
+	catch (std::exception &e)
 	{
-		std::cout << "Exception caught: " << e.what() << std::endl;
+		std::cout << "Caught exception: " << e.what() << std::endl;
 	}
+	catch (std::out_of_range &e)
+	{
+		std::cout << "Caught out_of_range exception: " << e.what() << std::endl;
+	}
+	// Test wrong type copy construction
+	std::cout << "***Test wrong type copy construction***\n" << std::endl;
+	std::cout << "Array<int> floatArrayCopy = intArray;" << std::endl;
+	std::cout << "error: redefinition of 'floatArrayCopy' with a different type: 'Array<int>' vs 'Array<double>'"
+			  << std::endl;
+	// Array<int> floatArrayCopy = intArray;
+	std::cout << std::endl;
 
+	std::cout << "*** Test access out of bounds ***" << std::endl;
+	std::cout << "Copied array size: " << floatArrayCopy.size() << std::endl;
+	try
+	{
+		std::cout << "floatArrayCopy[floatArrayCopy.size() - 1]: " << floatArrayCopy[floatArrayCopy.size() - 1]
+				  << std::endl;
+		std::cout << "floatArrayCopy[floatArrayCopy.size()]: " << floatArrayCopy[floatArrayCopy.size()] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "\nCaught exception: " << e.what() << std::endl;
+	}
+	catch (std::out_of_range &e)
+	{
+		std::cout << "\nCaught out_of_range exception: " << e.what() << std::endl;
+	}
+}
+
+int main()
+{
+	// testSimpleTypes();
+	testComplexType();
 	return 0;
 }
